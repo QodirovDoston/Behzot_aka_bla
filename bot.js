b8c611badac374d0afe870438d4b16c8abc7e8f6
QodirovDoston <qodirovdoston806@gmail.com>
2023-03-07 18:28:45 +0500

    efdf

--- bot.js
+++ bot.js
@@ -38,18 +38,16 @@
 
 // bot.launch();
 
-
-
 const { Telegraf } = require("telegraf");
 const TOKEN = "6139307692:AAH7O14oO-juIsdrnKcle632TmYXfGWkU0k";
 const bot = new Telegraf(TOKEN);
 const web_link = "https://6405bea2ada02800084117e0--stellular-narwhal-b3fe08.netlify.app//";
 
 bot.start((ctx) =>
-  ctx.reply(`Welcome ${ctx.from.first_name} Is there anything you want?`, {
+  ctx.reply(`Welcome ${ctx.from.first_name}🙋  Is there anything you want?`, {
     reply_markup: {
       keyboard: [[{ text: "order page", web_app: { url: web_link } }]],
     },
   })
-  );
+  )
 bot.launch();
