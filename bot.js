1fe5f6956c0c06c687a008476adf1228a941809d
QodirovDoston <qodirovdoston806@gmail.com>
2023-03-07 18:24:46 +0500

    dfsadf

--- bot.js
+++ bot.js
@@ -46,7 +46,6 @@ const bot = new Telegraf(TOKEN);
 const web_link = "https://6405bea2ada02800084117e0--stellular-narwhal-b3fe08.netlify.app//";
 
 bot.start((ctx) =>
-ctx.sendSticker('https://chpic.su/_data/stickers/b/BratishkinEmoji/BratishkinEmoji_001.webp'),
   ctx.reply(`Welcome ${ctx.from.first_name} Is there anything you want?`, {
     reply_markup: {
       keyboard: [[{ text: "order page", web_app: { url: web_link } }]],
